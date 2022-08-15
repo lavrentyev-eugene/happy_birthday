8f26b2de07373b3cb2bd6cdc6c9ad1380160ee82
Eugene Lavrentyev <65247513+lavrentyev-eugene@users.noreply.github.com>
2022-08-15 23:04:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ async def get_aircraft_flight_data(
 ):
     solution_hash = 'ab7a34ca99e00dbfe131be4c320b5f0578880defb62b8d29066fce2bfa96ebdc'
     if query_hash == solution_hash:
-        path = 'happy_birthday.png'
+        path = 'gift_card.pdf'
         return FileResponse(path)
     return Response('Wrong string, LUL)00))00))0')
 
